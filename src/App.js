cf5cf7094ed9687d1752fb5d20cf358bd2585f61
Kaan Bayram <kaan.9898@hotmail.com>
2020-01-06 13:40:55 +0300

    Portal Example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const appRoot = document.getElementById('root');   // 1
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.portalDiv = document.getElementById('react_portal');  //2
+  }
+
+  componentDidMount(){
+    appRoot.appendChild(this.portalDiv);  //3
+  }
+
+  componentWillUnmount(){
+    appRoot.removeChild(this.portalDiv);   //4
+  }
+
+
+  render() {
+    
+    return ( 
+      <div style={{visibility:'hidden'}}>   
+        {ReactDOM.createPortal(<div>this is a PORTAL</div>,this.portalDiv)}
+        <div>Invisible</div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+
+
